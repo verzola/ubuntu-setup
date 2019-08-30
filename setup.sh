cb764274c481156c6a36fb4f19d62ea63c780550
Gustavo Verzola <verzola@gmail.com>
2019-08-30 11:44:32 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -59,7 +59,7 @@ snap install skype --classic
 
 echo "Installing Google Chrome..."
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 apt update && apt install google-chrome-stable
 
 echo "Installing Docker..."
