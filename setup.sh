e4bdbb0c1150b4f9f657e326f3b78b0c3e911851
Gustavo Verzola <verzola@gmail.com>
2019-08-30 23:25:39 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -182,7 +182,7 @@ configure_tmux() {
 configure_vim() {
   step "Installing vim-plug"
   if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   fi
   check
