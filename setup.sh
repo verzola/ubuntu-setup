96051ec21e8180c1d588bf84473c6623070e3a6a
Gustavo Verzola <verzola@gmail.com>
2019-08-30 23:10:24 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -99,7 +99,7 @@ install_yarn() {
     warning "Yarn is already installed, skipping install"
   else
     curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-    sudo echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
     sudo apt update && sudo apt install -y yarn
     yarn --version
   fi
