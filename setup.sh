0b40a5601215f4245df719f87681f4ceeb24f25c
Gustavo Verzola <verzola@gmail.com>
2021-08-15 12:10:33 -0300

    fix: auto confirm typora install

--- setup.sh
+++ setup.sh
@@ -139,7 +139,7 @@ install_typora() {
   wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
   sudo add-apt-repository 'deb https://typora.io/linux ./'
   sudo apt-get update
-  sudo apt-get install typora
+  sudo apt-get install -y typora
 }
 
 configure_zsh() {
